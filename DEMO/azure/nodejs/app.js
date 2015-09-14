6f174fb32a305928b4616aee626fd2f45f593955
stankovski <denis.stankovski@gmail.com>
2015-09-14 09:21:34 -0700

    Updated demo script

--- DEMO/azure/nodejs/app.js
+++
@@ -1,8 +0,0 @@
-var msRestAzure = require('ms-rest-azure');
-
-var clientid = process.argv[2];
-var tenantid = process.argv[3];
-var secret = process.argv[4];
-
-var ResourceManagementClient = require('./generated/resourceManagementClient')
-var subscriptionCredential = new msRestAzure.SubscriptionCredentials()
